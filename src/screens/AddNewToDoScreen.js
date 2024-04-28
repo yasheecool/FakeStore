d4b93fc746f49007a184f5dc0c4e5c4c5e7ed851
Yash <yasheecool@gmail.com>
2024-04-28 16:07:17 +1000

    Category Screen Added

--- src/screens/AddNewToDoScreen.js
+++ src/screens/AddNewToDoScreen.js
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import Button from "../components/Button";
+import { Ionicons } from "@expo/vector-icons";
 
 const AddNewToDoScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Text style={styles.heading}>Add New To-do Item</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Choose Category</Text>
       </View>
 
-      <View></View>
+      <View style={styles.categoryContainer}>
+        <TouchableOpacity style={styles.category}>
+          <Ionicons name={"laptop-outline"} size={24} color={"black"} />
+          <Text>Electronics</Text>
+        </TouchableOpacity>
 
-      <View style={styles.btnContainer}>
-        <Button
-          style={styles.button}
-          onClickFn={navigation.goBack}
-          iconName={"arrow-back"}
-          text={"BACK"}
-        />
+        <TouchableOpacity style={styles.category}>
+          <Ionicons name={"diamond-outline"} size={24} color={"black"} />
+          <Text>Jewelry</Text>
+        </TouchableOpacity>
 
-        <Button
-          style={styles.button}
-          onClickFn={console.log("save btn clicked")}
-          iconName={"save-sharp"}
-          text={"SAVE"}
-        />
+        <TouchableOpacity style={styles.category}>
+          <Ionicons name={"shirt-sharp"} size={24} color={"black"} />
+          <Text>Men's Clothing</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.category}>
+          <Ionicons name={"woman-sharp"} size={24} color={"black"} />
+          <Text>Women's Clothing</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
@@ -33,34 +44,34 @@ const AddNewToDoScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-  },
-  headingContainer: {
+    backgroundColor: "#fa8ea2",
+    justifyContent: "flex-start",
     alignItems: "center",
-    backgroundColor: "#20A9FE",
-    justifyContent: "center",
   },
-  heading: {
-    padding: 10,
-    fontSize: 20,
-    fontWeight: "700",
-    color: "#FAFAFA",
+  titleContainer: {
+    marginTop: 50,
+    marginBottom: 125,
+  },
+  titleText: {
     fontFamily: "Helvetica",
+    fontWeight: "500",
+    fontSize: 30,
+  },
+  categoryContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
   },
 
-  btnContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-
-  button: {
-    flexDirection: "row",
+  category: {
+    backgroundColor: "#f5e0df",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#20A9FE",
-    padding: 10,
-    margin: 20,
-    borderRadius: 20,
+    borderColor: "black",
+    width: 150,
+    height: 150,
+    borderColor: "black",
+    borderWidth: 2,
   },
 });
 
